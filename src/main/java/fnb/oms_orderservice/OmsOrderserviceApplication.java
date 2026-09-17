9f58834e965494676a0a42cb03d2e6b8c83532f2
siphesih <maluleki01@gmail.com>
2026-09-17 15:09:53 +0200

    Initial commit

---
+++ src/main/java/fnb/oms_orderservice/OmsOrderserviceApplication.java
@@ -0,0 +1,13 @@
+package fnb.oms_orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsOrderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsOrderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fnb/oms_orderservice/OmsOrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package fnb.oms_orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsOrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
